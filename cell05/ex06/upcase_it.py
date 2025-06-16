376af46396b10120bf022afc78e025b6d3f8fc37
sittisak1 <oat38992@gmail.com>
2025-06-16 15:18:32 +0000

    upcase

---
+++ cell05/ex06/upcase_it.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    if len(sys.argv) == 2:
+        # แปลงพารามิเตอร์ตัวที่ 1 เป็นตัวพิมพ์ใหญ่
+        print(sys.argv[1].upper())
+    else:
+        print("none")
+
+if __name__ == "__main__":
+    main()
