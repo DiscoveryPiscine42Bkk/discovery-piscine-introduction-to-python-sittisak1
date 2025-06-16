35217df6b8eaf143e3c30573da214bf923981f97
sittisak1 <oat38992@gmail.com>
2025-06-16 14:28:44 +0000

    calculator

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    num1 = float(input("Give me the first number: "))
+    num2 = float(input("Give me the second number: "))
+    print("Thank you!")
+
+    print(f"{int(num1)} + {int(num2)} = {int(num1 + num2)}")
+    print(f"{int(num1)} - {int(num2)} = {int(num1 - num2)}")
+    print(f"{int(num1)} / {int(num2)} = {int(num1 / num2)}")
+    print(f"{int(num1)} * {int(num2)} = {int(num1 * num2)}")
+
+if __name__ == "__main__":
+    main()
