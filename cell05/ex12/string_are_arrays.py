983806b414e17f1d2b834c8475dd9f77628d87a2
sittisak1 <oat38992@gmail.com>
2025-06-16 15:53:16 +0000

    string

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+
+    text = sys.argv[1]
+    count_z = text.count('z')
+
+    if count_z == 0:
+        print("none")
+    else:
+        
+        print('z' * count_z)
+
+if __name__ == "__main__":
+    main()
