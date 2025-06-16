8740dbe472d45b2579eae89df8dc93390eca4d10
sittisak1 <oat38992@gmail.com>
2025-06-16 15:49:16 +0000

    paramet1

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+
+    param = sys.argv[1]
+    # รับ input จากผู้ใช้
+    answer = input("What was the parameter? ")
+
+    if answer == param:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+
+if __name__ == "__main__":
+    main()
