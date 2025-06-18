de4044cbfe62393aca5454ddbe332915390c3585
sittisak1 <oat38992@gmail.com>
2025-06-18 09:24:39 +0000

    ezzzz

--- cell05/ex06/upcase_it.py
+++ cell05/ex06/upcase_it.py
@@ -4,7 +4,7 @@ def main():
     if len(sys.argv) == 2:
         print(sys.argv[1].upper())
     else:
-        print("RTFM (Read the F-ing manual)")
+        print("none")
 
 if __name__ == "__main__":
     main()
--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) == 2:
         print(sys.argv[1].lower())
     else:
-        print("RTFM (Read the F-ing manual)")
+        print("none")
 
 if __name__ == "__main__":
     main()
