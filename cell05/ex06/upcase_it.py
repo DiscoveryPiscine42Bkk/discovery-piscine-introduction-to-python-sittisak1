2a25ebe588a250ab67913e750fb4d5d835c939fe
sittisak1 <oat38992@gmail.com>
2025-06-18 05:14:49 +0000

    kk

--- cell05/ex06/upcase_it.py
+++ cell05/ex06/upcase_it.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
 import sys
 
 def main():
     if len(sys.argv) == 2:
-        # แปลงพารามิเตอร์ตัวที่ 1 เป็นตัวพิมพ์ใหญ่
         print(sys.argv[1].upper())
     else:
         print("RTFM (Read the F-ing manual)")
