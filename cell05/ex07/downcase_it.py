6d9c6b22c250517b573042ba2993326b2f84cc53
sittisak1 <oat38992@gmail.com>
2025-06-18 05:16:26 +0000

    gghh

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) == 2:
         print(sys.argv[1].lower())
     else:
-        print("none")
+        print("RTFM (Read the F-ing manual)")
 
 if __name__ == "__main__":
     main()
